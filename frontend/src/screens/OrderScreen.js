4a1c626e583cd863ec99826a0bd71e97aa093e71
phn921 <panghenam@gmail.com>
2021-11-05 04:23:04 +0800

    paypalupdate

--- frontend/src/screens/OrderScreen.js
+++ frontend/src/screens/OrderScreen.js
@@ -3,15 +3,23 @@ import {PayPalButton} from 'react-paypal-button-v2'
 import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
-import { detailsOrder } from '../actions/orderActions';
+import { detailsOrder, payOrder } from '../actions/orderActions';
 import LoadingBox from '../components/Loadingbox';
 import MessageBox from '../components/MessageBox';
+import { ORDER_PAY_RESET } from '../constants/orderConstants';
 
 export default function OrderScreen(props) {
     const orderId =props.match.params.id;
     const [sdkReady, setSdkReady]= useState(false);
     const orderDetails = useSelector((state) => state.orderDetails);
     const { order, loading, error}=orderDetails;
+    
+    const orderPay =useSelector((state) => state.orderPay);
+    const {
+        loading: loadingPay, 
+        error: errorPay, 
+        success: successPay
+     } = orderPay;
     const dispatch = useDispatch();
     useEffect(() => {
       const addPayPalScript = async () =>{
@@ -25,9 +33,10 @@ export default function OrderScreen(props) {
           };
           document.body.appendChild(script);
       };
-      if (!order._id){
+      if (!order || successPay || (order && order._id !== orderId)){
+        dispatch({type:ORDER_PAY_RESET});
         dispatch(detailsOrder(orderId));
-      }else{
+      } else {
           if(!order.isPaid){
               if(!window.paypal){
                   addPayPalScript();
@@ -37,10 +46,10 @@ export default function OrderScreen(props) {
           }
       }
       
-    },[dispatch, order, orderId, sdkReady ]);
+    },[dispatch, order, orderId, sdkReady, successPay ]);
     
-    const successPaymentHandler=() =>{
-
+    const successPaymentHandler=(paymentResult) =>{
+      dispatch(payOrder(order, paymentResult));
     }
     
     return  loading
@@ -146,10 +155,20 @@ export default function OrderScreen(props) {
                              {
                                  !order.isPaid && (
                                      <li>
-                                         { !sdkReady? (<LoadingBox></LoadingBox>):
-                                         (
-                                             <PayPalButton amount={order.totalPrice} onSuccess={successPaymentHandler}>
-                                             </PayPalButton>    
+                                         { !sdkReady? (
+                                            <LoadingBox></LoadingBox>
+                                         ):(
+                                             <>
+                                             {errorPay && (
+                                                <MessageBox variant = "danger">{errorPay}</MessageBox>
+                                              )}
+                                              {loadingPay && <LoadingBox></LoadingBox>}
+
+                                             <PayPalButton
+                                              amount={order.totalPrice} 
+                                              onSuccess={successPaymentHandler}
+                                              ></PayPalButton>    
+                                             </>
                                          )
                                          }
                                      </li>
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,7 +1,7 @@
 import { createStore, compose, applyMiddleware, combineReducers} from 'redux';
 import thunk from 'redux-thunk';
 import { cartReducer } from './reducers/cartReducers';
-import { orderCreateReducer, orderDetailsReducer } from './reducers/orderReducers';
+import { orderCreateReducer, orderDetailsReducer, orderPayReducer } from './reducers/orderReducers';
 import { productDetailsReducer, productListReducer } from './reducers/productReducer';
 import { userRegisterReducer, userSigninReducer } from './reducers/userReducers';
 
@@ -28,7 +28,8 @@ const reducer = combineReducers({
     userSignin:userSigninReducer,
     userRegister:userRegisterReducer,
     orderCreate:orderCreateReducer,
-    orderDetails:orderDetailsReducer
+    orderDetails:orderDetailsReducer,
+    orderPay:orderPayReducer,
 });
 
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENTION_COMPOSE__ || compose;
